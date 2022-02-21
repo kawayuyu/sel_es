f933fe72bdaa5236e833091c7855c356014e1f24
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-02-21 16:49:03 +0900

    テーブル作成
    テーブル作成
    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname, uniqueness: true, length: { maximum: 8 }
+    validates :birthday
+    validates :email, uniqueness: true
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+  end
 end
