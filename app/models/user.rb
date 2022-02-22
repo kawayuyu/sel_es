a4e001cbfb90a81eb6bd9137cd0aa3d0e03ba87c
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-02-22 15:23:09 +0900

    エラーメッセージ日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
     validates :birthday
     validates :email, uniqueness: true
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+    validates_format_of :password, with: PASSWORD_REGEX
   end
 end
