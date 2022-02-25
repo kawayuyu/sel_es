59c56cdc5204e93c72769a00f0ef80510d786b61
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-02-25 18:10:04 +0900

    コントローラーの設定
    ビューファイルの作成

---
+++ app/controllers/rules_controller.rb
@@ -0,0 +1,45 @@
+class RulesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_rule, only: [:edit, :show, :destroy, :update]
+
+  def index
+    @rules = current_user.rules.all.order(created_at: :desc)
+  end
+
+  def new
+    @rule = Rule.new
+  end
+
+  def create
+    @rule = Rule.new(rule_params)
+    if @rule.save
+      redirect_to rules_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @rule.destroy
+  end
+
+  def edit
+  end
+
+  def update
+    @rule.update(rule_params)
+  end
+
+  def show
+  end
+
+  private
+
+  def rule_params
+    params.require(:rule).permit(:if, :then).merge(user_id: current_user.id)
+  end
+
+  def set_rule
+    @rule = current_user.rules.find(params[:id])
+  end
+end
