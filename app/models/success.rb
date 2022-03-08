58c2d1f0b14847274253e93dfae58fe5bc1c78d5
kawayuyu <96399944+kawayuyu@users.noreply.github.com>
2022-03-08 14:27:22 +0900

    バリデーションの追記
    NGリストについては未実装
    追加で実装予定

--- app/models/success.rb
+++ app/models/success.rb
@@ -1,5 +1,10 @@
 class Success < ApplicationRecord
   belongs_to :user
 
-  validates :post, presence: true
+  NGWORD_REGEX = /(.)\1{4,}/.freeze
+  with_options presence: true do  
+   with_options format: { without: NGWORD_REGEX, message: 'は5文字以上の連続する文字は禁止です' } do
+    validates :post, obscenity: { message: 'にNGワードが入力されています' }
+   end
+  end
 end
